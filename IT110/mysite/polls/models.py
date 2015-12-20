4b8f58b936aa29ec81386060cafc9253bd45a3ca
Sa <Sa>
2015-12-20 15:56:13 -0500

    finagled with part 7, assignment complete

--- IT110/mysite/polls/models.py
+++ IT110/mysite/polls/models.py
@@ -12,6 +12,9 @@ def __str__(self):
 def was_published_recently(self):
     now = timezone.now()
     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
